4622d94db57f236f65d2ad625b06f78717e99541
ILaxaiI <undefinednameexception@gmail.com>
2023-05-03 11:50:47 +0200

    Robot Move Function

--- Assets/Code/Scripts/RobotScript.cs
+++ Assets/Code/Scripts/RobotScript.cs
@@ -3,22 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
+
 public class RobotScript: MonoBehaviour
 {
-    public void Move()
+
+    [SerializeField]
+    //Movement Speed of the Robot.
+    private float Speed = 20f;
+
+    private bool isRunning = false;
+    private Queue<IEnumerator> Tasks = new Queue<IEnumerator>();
+
+    //Moves Robot by Lerping between start and end position
+    IEnumerator _Move(float x, float y)
     {
-        InvokeRepeating("_Move", 0, Time.deltaTime);
+        
+        isRunning = true;
+        if(x != 0 || y != 0) {
+        
+        float dist = Mathf.Sqrt(x*x+y*y);
+        float time = dist/Speed;
+
+        Vector3 Start = transform.position;
+        Vector3 End = Start + new Vector3(x, 0 ,y);
+
+        float t = 0;
+        while(t < time)
+        {
+            transform.position = Vector3.Lerp(Start,End,t/time);
+            t = t + Time.deltaTime;
+            yield return null;
+        }
+
+        //Prevents rounding errors and imprecision
+        transform.position = End;
+        }
+        isRunning = false;
     }
 
-    public void MoveTo(int x, int y)
-    {
-        Grid grid = GameObject.Find("Grid").GetComponent<Grid>();
-        gameObject.transform.Translate(grid.CellToWorld(new Vector3Int(x, y, 0)));
+    public void Move(float x, float y){Tasks.Enqueue(_Move(x,y));}
+    public void Move(int x, int y){Tasks.Enqueue(_Move((float)x,(float)y));}
 
+    public void Start() {
+    
+        for (int i = 0; i < 2; i++) {
+            Move(1,0);
+        }
+
+        Move(1f,2f);
+        Move(-1f,-2f);
     }
 
-    private void _Move ()
-    {
-        gameObject.transform.Translate(Vector3.forward * 0.1f * Time.deltaTime);
+    public void Update() {
+        if(Tasks.Count>0 && Tasks.Peek() != null && !isRunning){
+            StartCoroutine(Tasks.Dequeue());
+        }
+        else if(!isRunning && Tasks.Count <= 0)
+        {
+            //Debug.Log("Done!");
+        }
     }
 }
