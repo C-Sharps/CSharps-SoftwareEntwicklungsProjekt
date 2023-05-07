5e2347be2a1a2eed4efff9c26ef21f297c6be22f
Stefan Pietzner <stefan.pietzner@gmail.com>
2023-05-07 20:54:50 +0200

    Added Tools/Grid Generator menu item

--- Assets/Editor/GridGeneratorEditor.cs
+++ Assets/Editor/GridGeneratorEditor.cs
@@ -1,26 +1,59 @@
 using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
-
-
 
 /*  Custom editor for the GridGenerator
  */
 public class GridGeneratorEditor : EditorWindow
+
 {
+    private PropertyField gridObject;
+    private Vector2IntField gridSizeField;
+    private Vector3Field originField;
+    private PropertyField tileSpriteField;
+    private FloatField cellSizeXField;
+    private FloatField cellSizeYField;
+    private FloatField cellSizeZField;
+    private EnumField orientationField;
+    private Toggle useGridLabelToggle;
+    private Button gridGeneratorButton;
 
-    public VisualTreeAsset gridEditorInspectorXML;
+    private GridGenerator gridGenerator;
 
-    public void CreateGUI()
+    void OnAwake()
     {
-        VisualElement gridEditorInspector = new VisualElement();
-        gridEditorInspectorXML.CloneTree(gridEditorInspector);
+        gridGenerator ??= new GridGenerator();
     }
 
     [MenuItem("Tools/Grid Generator")]
-    public static void ShowEditor()
+    public static void ShowWindow()
+        {
+            EditorWindow window = GetWindow<GridGeneratorEditor>();
+            window.titleContent = new GUIContent("Grid Generator");
+        }
+
+    public void CreateGUI()
     {
-        EditorWindow window = GetWindow<GridGeneratorEditor>();
-        window.titleContent = new UnityEngine.GUIContent("Grid Generator");
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/GridEditorLayout.uxml");
+        var layout = visualTree.Instantiate();
+        rootVisualElement.Add(layout);
+
+        gridObject = rootVisualElement.Query<PropertyField>("gridObject");
+        gridSizeField = rootVisualElement.Query<Vector2IntField>("gridSizeField");
+        originField = rootVisualElement.Query<Vector3Field>("originField");
+        tileSpriteField = rootVisualElement.Query<PropertyField>("tileSpriteField");
+        cellSizeXField = rootVisualElement.Query<FloatField>("cellSizeXField");
+        cellSizeYField = rootVisualElement.Query<FloatField>("cellSizeYField");
+        cellSizeZField = rootVisualElement.Query<FloatField>("cellSizeZField");
+        orientationField = rootVisualElement.Query<EnumField>("orientationField");
+        useGridLabelToggle = rootVisualElement.Query<Toggle>("useGridLabelToggle");
+        gridGeneratorButton = rootVisualElement.Query<Button>("gridGeneratorButton");
+        gridGeneratorButton.clicked += Generate;
+    }
+
+    private void Generate()
+    {
+        gridGenerator.GenerateGrid();
     }
 }
--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -4,46 +4,54 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using System.Runtime.CompilerServices;
+using UnityEngine.Tilemaps;
 
-public class GridGenerator : MonoBehaviour
+public class GridGenerator : ScriptableObject
 {
-    [SerializeField]
+    public string gridName;
     private GameObject gridObject;
-    [SerializeField]
     private Grid grid;
+    public Sprite tileSprite;
+    private Tilemap tilemap;
     public Vector3 origin;
     public GridOrientation orientation;
     public Vector2Int gridSize;
     public float cellSizeX = 1f;
     public float cellSizeY = 1f;
     public bool useLabels;
+    private Canvas canvas;
     public TMP_Text gridLabel;
     public delegate void Function();
     public Function generateGrid;
 
-    private void OnValidate()
-    {
-       GenerateGrid();
-    }
-
     public void GenerateGrid()
     {
-        if (gridObject == null)
+        if (gridObject != null)
         {
-            gridObject = new GameObject("Grid");
-            Quaternion rotation = GetRotationForOrientation(orientation);
-            gridObject.transform.rotation = rotation;
-            //Instantiate(gridObject, origin, rotation);
+            Object.Destroy(gridObject);
         }
 
+        if (gridName.NullIfEmpty().IsUnityNull())
+        {
+            gridName = "Grid";
+        }
+
+        gridObject = new GameObject(gridName);
+        Quaternion rotation = GetRotationForOrientation(orientation);
+        gridObject.transform.rotation = rotation;
         grid = gridObject.AddComponent<Grid>();
         grid.cellSize = new Vector3(cellSizeX, 1f, cellSizeY);
         gridObject.transform.position = origin;
 
         if (useLabels)
         {
-            Canvas canvas = grid.AddComponent<Canvas>();
-            GridText.setGridText(grid, canvas, gridLabel, gridSize.x, gridSize.y);
+            canvas = grid.AddComponent<Canvas>();
+            SetGridText();
+        }
+
+        if (tileSprite != null)
+        {
+            FillGridWithTiles();
         }
     }
 
@@ -57,6 +65,35 @@ public class GridGenerator : MonoBehaviour
             case GridOrientation.XY: return Quaternion.identity;
             case GridOrientation.YZ: return Quaternion.Euler(0f, -90f, 0);
             default: return Quaternion.identity;
+        }
+    }
+
+    public void SetGridText()
+    {
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                TMP_Text newlabel = Object.Instantiate(gridLabel);
+                newlabel.rectTransform.SetParent(canvas.transform, false);
+                newlabel.rectTransform.anchoredPosition = new Vector2(x + 0.5f, y + 0.5f);
+                newlabel.text = x + ", " + y;
+            }
+        }
+    }
+
+    private void FillGridWithTiles()
+    {
+        tilemap = gridObject.AddComponent<Tilemap>();
+        Tile tile = ScriptableObject.CreateInstance<Tile>();
+        tile.sprite = tileSprite;
+
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; y < gridSize.x; x++)
+            {
+                tilemap.SetTile(new Vector3Int(x, y), tile);
+            }
         }
     }
 }
