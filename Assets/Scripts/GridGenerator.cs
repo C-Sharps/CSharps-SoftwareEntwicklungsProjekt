ff4f2438fe90d767fd73850da4f5314f8102838a
Stefan Pietzner <stefan.pietzner@gmail.com>
2023-05-11 18:33:36 +0200

    implemented GridEditor tool

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -8,22 +8,26 @@ using UnityEngine.Tilemaps;
 
 public class GridGenerator : ScriptableObject
 {
-    public string gridName;
+    public string gridName = "Grid";
     private GameObject gridObject;
     [SerializeField]
     private Grid grid;
     public Sprite tileSprite;
-    private Tilemap tilemap;
     public Vector3 origin;
     public GridOrientation orientation;
-    public Vector2Int gridSize;
+    public Vector2Int gridSize = Vector2Int.one;
     public float cellSizeX = 1f;
     public float cellSizeY = 1f;
-    public bool useLabels;
-    private TMP_Text gridLabel;
+    public bool shallUseLabels;
+    public bool shallCreateTilemap;
 
     public void GenerateGrid() 
     {
+        if (gridSize.x <= 0 || gridSize.y <= 0)
+        {
+            throw new GridGeneratorException("Cannot set size of grid dimensions (x, y) to zero or negative numbers!");
+        }
+
         if (gridName.NullIfEmpty().IsUnityNull())
         {
             gridName = "Grid";
@@ -36,12 +40,27 @@ public class GridGenerator : ScriptableObject
         grid.cellSize = new Vector3(cellSizeX, 1f, cellSizeY);
         gridObject.transform.position = origin;
 
-        if (useLabels)
+        if (shallUseLabels)
         {
             GameObject canvasObject = new GameObject("Canvas", typeof(Canvas));
-            canvasObject.transform.SetParent(gridObject.transform);
             Canvas canvas = canvasObject.GetComponent<Canvas>();
+            RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+
+            // Setting the canvas's anchor and pivot to the lower left corner
+            canvasRect.pivot= Vector2.zero;
+            canvasRect.anchoredPosition = Vector2.zero;
+
+            // Resizing the canvas to match the grid
+            canvasRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, gridSize.x * cellSizeX);
+            canvasRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, gridSize.y * cellSizeY);
+
+            canvasObject.transform.SetParent(gridObject.transform, false);
             SetGridText(canvas);
+        }
+
+        if (shallCreateTilemap)
+        {
+            CreateTilemap();
         }
 
         if (tileSprite != null)
@@ -65,33 +84,46 @@ public class GridGenerator : ScriptableObject
 
     public void SetGridText(Canvas canvas)
     {
-
-        TMP_Text gridLabel = Resources.Load<TMP_Text>("Assets/Prefabs/GridLabel");
+        // Caution: Assets must be in the "Resources" folder to be able to load them with Resources.Load()!
+        TMP_Text gridLabel = Resources.Load<TMP_Text>("GridLabel");
+        Vector3 offset = new Vector3(cellSizeX / 2, cellSizeY / 2);
 
         for (int y = 0; y < gridSize.y; y++)
         {
             for (int x = 0; x < gridSize.x; x++)
             {
-                TMP_Text newlabel = Object.Instantiate(gridLabel);
-                newlabel.rectTransform.SetParent(canvas.transform, false);
-                newlabel.rectTransform.anchoredPosition = new Vector2(x + 0.5f, y + 0.5f);
-                newlabel.text = x + ", " + y;
+                TMP_Text newLabel = Instantiate(gridLabel);
+                newLabel.gameObject.name = "(" + x + " ," + y + ")";
+                newLabel.rectTransform.SetParent(canvas.transform, false);
+                newLabel.rectTransform.localPosition = grid.CellToLocal(new Vector3Int(x, y)) + offset;
+                newLabel.text = x + ", " + y;
             }
         }
     }
 
+    private void CreateTilemap()
+    {
+        GameObject tilemapObject = new GameObject("Tilemap", typeof(Tilemap));
+        tilemapObject.transform.SetParent(gridObject.transform, false);
+        tilemapObject.AddComponent<TilemapRenderer>();
+    }
+
     private void FillGridWithTiles()
     {
-        tilemap = gridObject.AddComponent<Tilemap>();
         Tile tile = CreateInstance<Tile>();
         tile.sprite = tileSprite;
+        Tilemap tilemap = gridObject.GetComponentInChildren<Tilemap>();
 
         for (int y = 0; y < gridSize.y; y++)
         {
-            for (int x = 0; y < gridSize.x; x++)
+            for (int x = 0; x < gridSize.x; x++)
             {
                 tilemap.SetTile(new Vector3Int(x, y), tile);
             }
         }
+    }
+
+    private class GridGeneratorException : UnityException { 
+        public GridGeneratorException(string message) : base(message) { }
     }
 }
