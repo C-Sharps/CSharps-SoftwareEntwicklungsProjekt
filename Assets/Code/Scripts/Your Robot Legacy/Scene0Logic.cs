6f63d73d5124933c84c2899a6a1fe7ed7544d655
MrFleckstone <max.fleckenstein.work@gmail.com>
2023-05-28 11:35:21 +0200

    reverted  faulty scene0 changes

--- Assets/Code/Scripts/Your Robot Legacy/Scene0Logic.cs
+++ Assets/Code/Scripts/Your Robot Legacy/Scene0Logic.cs
@@ -9,7 +9,6 @@ using System.Linq;
 
 public class Scene0Logic : SceneLogic
 {
-    /*
     GameObject[] robots;
     public void Update()
     {
@@ -24,20 +23,5 @@ public class Scene0Logic : SceneLogic
         {
             CompleteObjective(1);
         }
-    }
-    */
-
-    GameObject[] robots;
-    public void Update()
-    {
-        // ToDo: Not performant, should be done with events
-        robots = GameObject.FindGameObjectsWithTag("Player");
-        if (robots.Length >= 10 && lessonObjectives[0].isCompleted == false)
-        {
-            
-            CompleteObjective(0);
-        }
-
-        print(robots);
     }
 }
