1bf16b38ec291abe4a49aa21c34a366cb2a55c94
MrFleckstone <max.fleckenstein.work@gmail.com>
2023-06-04 20:21:54 +0200

    renamed some files and added logic

---
+++ Assets/Code/Scripts/Scenes/Scene_2/Scene2Logic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Code.Scripts;
+using TMPro;
+using UnityEngine;
+using RoslynCSharp;
+
+public class Scene1Logic : SceneLogic
+{
+    // ToDo: Not performant, should be done with events
+    GameObject[] robots;
+    public void Update()
+    {
+      
+    }
+}
