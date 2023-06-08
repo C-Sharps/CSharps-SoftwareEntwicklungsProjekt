7199de7d4cfa6bb8091388a50085fa6eeed9c632
MrFleckstone <max.fleckenstein.work@gmail.com>
2023-06-08 20:17:04 +0200

    added box constructors and finished objectives and classes for lesson 2

--- Assets/Code/Scripts/Scenes/Scene_2/Scene2Logic.cs
+++ Assets/Code/Scripts/Scenes/Scene_2/Scene2Logic.cs
@@ -2,16 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Code.Scripts;
+using Code.Scripts.System.SceneManager;
 using TMPro;
 using UnityEngine;
 using RoslynCSharp;
+using System.Linq;
 
 public class Scene2Logic : MonoBehaviour
 {
-    // ToDo: Not performant, should be done with events
-    GameObject[] robots;
+    GameObject[] boxes;
+
+    private LessonController _controller;
+
+    public void Awake()
+    {
+        _controller = FindObjectOfType<LessonController>();
+    }
+
     public void Update()
     {
-      
+        boxes = GameObject.FindGameObjectsWithTag("Box");
+        if (boxes.Any(obj => obj != null) && _controller.data.lessonObjectives[0].isCompleted == false)
+        {
+            _controller.CompleteObjective(0);
+        }
     }
 }
