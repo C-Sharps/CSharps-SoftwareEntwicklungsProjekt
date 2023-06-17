960f7de8e3929bb324e75e8f370592ba3c6a15e9
Stefan Pietzner <stefan.pietzner@gmail.com>
2023-06-17 18:00:47 +0200

    pre-merge commit

--- Assets/Code/Scripts/Robot/Modules/Legs.cs
+++ Assets/Code/Scripts/Robot/Modules/Legs.cs
@@ -41,22 +41,23 @@ public class Legs : MonoBehaviour
         _isRunning = true;
         if(x != 0 || y != 0) {
         
-        float dist = Mathf.Sqrt(x*x+y*y);
-        float time = dist/_Speed;
+            float dist = Mathf.Sqrt(x*x+y*y);
+            float time = dist/_Speed;
 
-        Vector3 Start = transform.position;
-        Vector3 End = Start + new Vector3(x, 0 ,y);
+            Vector3 Start = transform.position;
+            Vector3 End = Start + new Vector3(x, 0 ,y);
+            transform.LookAt(End);
 
-        float t = 0;
-        while(t < time)
-        {
-            transform.position = Vector3.Lerp(Start, End, t/time);
-            t += Time.deltaTime;
-            yield return null;
-        }
+            float t = 0;
+            while(t < time)
+            {
+                transform.position = Vector3.Lerp(Start, End, t/time);
+                t += Time.deltaTime;
+                yield return null;
+            }
 
-        // Prevents rounding errors and imprecision
-        transform.position = End;
+            // Prevents rounding errors and imprecision
+            transform.position = End;
         }
         _isRunning = false;
     }
