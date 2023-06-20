31253eb181e383270789b4d9011096938739f812
MrFleckstone <max.fleckenstein.work@gmail.com>
2023-06-20 21:54:23 +0200

    ???

---
+++ Assets/Code/Scripts/SceneScripts/Lesson04/Scene4Logic.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Code.Scripts;
+using Code.Scripts.System.SceneManager;
+using TMPro;
+using UnityEngine;
+using RoslynCSharp;
+using System.Linq;
+
+public class Scene4Logic : MonoBehaviour
+{
+    GameObject[] boxes;
+
+    private LessonController _controller;
+    private GameObject _boxPrefab;
+
+    private GameObject _box1;
+    private GameObject _box2;
+    
+    public void Awake()
+    {
+        _controller = FindObjectOfType<LessonController>();
+
+        _boxPrefab = Resources.Load<GameObject>("Prefabs/Box");
+    }
+
+    public void Start()
+    {
+        _box1 = Instantiate(_boxPrefab, new Vector3(5.75f, 4f, 0f),
+            Quaternion.identity);
+        _box1.transform.localScale = new Vector3(2, 2, 2);
+        _box1.GetComponentsInChildren<MeshRenderer>()[0].material.color = Color.red;
+        _box1.transform.Rotate(0f, 180f, 0f);
+
+        _box2 = Instantiate(_boxPrefab, new Vector3(5.75f, 10f, 0f),
+            Quaternion.identity);
+        _box2.transform.localScale = new Vector3(1.8f, 1.8f, 1.8f);
+        _box2.GetComponentsInChildren<MeshRenderer>()[0].material.color = Color.yellow;
+        _box2.transform.Rotate(0f, 180f, 0f);
+    }
+
+    public void Update()
+    {
+        boxes = GameObject.FindGameObjectsWithTag("Box(weight, scale)");
+        if (boxes.Length > 2)
+        {
+            ShrinkAndDestroyBox();
+        }
+
+        if (boxes.Length == 2)
+        {
+            if(!CheckBoxesEqual())
+            {
+                if (_controller.data.lessonObjectives[0].isCompleted == false)
+                {
+                    _controller.CompleteObjective(0);
+                }
+            }
+        }
+    }
+
+    private void ShrinkAndDestroyBox()
+    {
+        GameObject box = boxes[0];
+
+        if (box.transform.localScale.magnitude > 0.1f)
+        {
+            float shrinkAmount = 1.0f;
+
+            if (boxes.Length > 3)
+            {
+                shrinkAmount = 1.5f * boxes.Length;
+            }
+
+            Vector3 newScale = box.transform.localScale - new Vector3(shrinkAmount, shrinkAmount, shrinkAmount) * Time.deltaTime;
+
+            newScale = Vector3.Max(newScale, Vector3.zero);
+
+            box.transform.localScale = newScale;
+        }
+        else
+        {
+            Destroy(box);
+        }
+    }
+
+    private bool CheckBoxesEqual()
+    {
+        return (boxes[0].GetComponent<Rigidbody>().mass == boxes[1].GetComponent<Rigidbody>().mass &&
+        boxes[0].transform.localScale == boxes[1].transform.localScale &&
+        boxes[0].GetComponentsInChildren<MeshRenderer>()[0].material.color == boxes[1].GetComponentsInChildren<MeshRenderer>()[0].material.color);
+    }
+
+    public GameObject GetRedBox()
+    {
+        return _box1;
+    } 
+
+    public GameObject GetYellowBox()
+    {
+        return _box2;
+    } 
+
+    public float GetBoxSize(GameObject box)
+    {
+        return box.transform.localScale.x;
+    }
+}
